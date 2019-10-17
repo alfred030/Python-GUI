d9dbb14d85b66a503d5c12a11d07fada7138ad80
Tia Leonard <56228669+leonardtia1@users.noreply.github.com>
2019-10-17 12:41:33 -0500

    Add files via upload

---
+++ Frames/frame 2.py
@@ -0,0 +1,28 @@
+
+from tkinter import *
+from tkinter import ttk        
+    
+root = Tk()
+
+frame = ttk.Frame(root)
+frame.pack()
+
+frame.config(height = 100, width = 200)
+# there 6 different type of frames
+# FLAT, RAISED, SUNKEN, SOLID, RIDGE, GROOVE
+frame.config(relief = RAISED)
+
+button = ttk.Button(frame, text = 'Rescare SEA')
+button.pack()
+# x direction is 10 and y direction is 10
+frame.config(padding = (10, 10))
+
+logo = PhotoImage(file = 'tour_logo1.gif')
+# text position, we can use TOP, RIGHT,LEFT, and BOTTOM
+button.config(image = logo, compound = BOTTOM)
+small_logo = logo.subsample(1,1)
+button.config(ima = small_logo)
+
+ttk.LabelFrame(root, height = 100, width = 300, text = 'My Frame').pack()
+
+root.mainloop()
